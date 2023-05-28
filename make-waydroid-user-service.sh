eff70370c90f0206bf2b9206ed3dc2eb9311e34f
youdontknowdemo <36462734+youdontknowdemo@users.noreply.github.com>
2023-05-27 22:55:27 -0600

    Update make-waydroid-user-service.sh

--- make-waydroid-user-service.sh
+++ make-waydroid-user-service.sh
@@ -1,6 +1,19 @@
+#!/bin/bash
 ##################################################################################
 ##### WILL NOT WORK UNLESS: waydroid prop set persist.waydroid.multi_windows flase
 ##################################################################################
+
+# Variables
+WAYDROID_SERVICE_NAME="waydroid-user"
+WAYDROID_BINARY_PATH="/usr/bin/waydroid"
+WAYDROID_OPTIONS="session start"
+SERVICE_FILE="$WAYDROID_SERVICE_NAME.service"
+SERVICE_DIR="$HOME/.config/systemd/user"
+SERVICE_PATH="$SERVICE_DIR/$SERVICE_FILE"
+
+# Create the service directory if it doesn't exist
+mkdir -p "$SERVICE_DIR"
+
 # Create the service file
 cat << EOF > "$SERVICE_PATH"
 [Unit]
@@ -8,7 +21,7 @@ Description=Waydroid Service
 After=network.target
 
 [Service]
-ExecStart=/bin/bash -c 'while pgrep -x "waydroid.com.google.android.youtube.kids" >/dev/null; do sleep 3; done'
+ExecStart=/bin/bash -c 'while pgrep -x "waydroid.com.google.android.apps.youtube.kids" >/dev/null; do sleep 3; done'
 ExecStartPost=-/bin/bash -c 'systemd-run --user --scope /usr/bin/waydroid app launch com.google.android.apps.youtube.kids'
 Restart=always
 RestartSec=3
